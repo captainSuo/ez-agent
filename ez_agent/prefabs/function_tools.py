fdebb2c3ae9a02152b63d48a64e32d049a077f40
captainSuo <captainsuo@outlook.com>
2025-09-27 17:38:33 +0800

    refactor: 删除不好用的工具

--- ez_agent/prefabs/function_tools.py
+++ ez_agent/prefabs/function_tools.py
@@ -7,9 +7,7 @@ def python_script_tool(script: str) -> str:
     """A tool that allows you to run python script and returns the full output. Tips: You can use `print` to check the result"""
     import subprocess
 
-    result = subprocess.run(
-        ["python", "-E", "-c", script], capture_output=True, text=True
-    )
+    result = subprocess.run(["python", "-E", "-c", script], capture_output=True, text=True)
     return result.stdout + result.stderr
 
 
@@ -28,16 +26,3 @@ def open_website_tool(url: str) -> str:
 
     webbrowser.open(url)
     return f"Successfully opened {url}"
-
-
-@FoldableFunctionTool
-def browse_web_tool(url: str) -> str:
-    """A tool that allows you to browse the website"""
-    import httpx, bs4  # type: ignore
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-    }
-    response = httpx.get(url, headers=headers)
-    soup = bs4.BeautifulSoup(response.text, "html.parser")
-    return soup.get_text()
